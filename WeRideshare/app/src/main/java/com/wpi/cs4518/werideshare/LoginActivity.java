f5a8e2992f4ba8ed473b095f92bbd06d03a7f30a
Will Wen <will.wen1@gmail.com>
2016-11-13 23:19:51 -0500

    added JSON database with simple write functionality

--- WeRideshare/app/src/main/java/com/wpi/cs4518/werideshare/LoginActivity.java
+++ WeRideshare/app/src/main/java/com/wpi/cs4518/werideshare/LoginActivity.java
@@ -31,6 +31,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.sql.Connection;
@@ -333,8 +338,31 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // TODO: attempt authentication against a network service.
 
             try {
+                //https://firebase.google.com/docs/database/android/start/
                 // Simulate network access.
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("user");
 
+                myRef.setValue(new User(mEmail, mPassword));
+
+
+                // Read from the database
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        String value = dataSnapshot.getValue(String.class);
+                        Log.d(TAG, "Value is: " + value);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Log.w(TAG, "Failed to read value.", error.toException());
+                    }
+                });
 
             } catch (Exception e)  {
                 return false;
