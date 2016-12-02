5148c502e1b2740dab77784530079e77f65ec2a0
mrampiah <mr.ampiah@gmail.com>
2016-12-02 15:54:35 -0500

    fixes to registration and profile activities

--- WeRideshare/app/src/main/java/com/wpi/cs4518/werideshare/fragments/VehicleDetailsFragment.java
+++ WeRideshare/app/src/main/java/com/wpi/cs4518/werideshare/fragments/VehicleDetailsFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.wpi.cs4518.werideshare.R;
 
@@ -25,5 +27,23 @@ public class VehicleDetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_vehicle_details, container, false);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        loadSpinnerCapacity();
+    }
+
+
+    private void loadSpinnerCapacity() {
+        Spinner spinner = (Spinner) getView().findViewById(R.id.capacitySpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.VehicleCapacity, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
